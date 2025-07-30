390e192712b24cc6dd445f6a64877a8a3655435a
Luka <lekishvililuka76@gmail.com>
2025-07-30 17:50:50 +0400

    start

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,88 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import discordIcon from "@/public/discord.png";
+import facebookIcon from "@/public/facebook.png";
+import emailIcon from "@/public/email.png";
 
 function Nav() {
-  return <div>Nav</div>;
+  const [dark, setDark] = useState(false);
+  const [isPagesOpened, setIsPagesOpened] = useState(false);
+  const [isSocialsOpened, setIsSocialsOpened] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", dark);
+  }, [dark]);
+  return (
+    <div className="flex bg-primary h-30 justify-between">
+      <button
+        onClick={() => setIsPagesOpened(!isPagesOpened)}
+        className="bg-primary p-1 rounded-xs"
+      >
+        pages
+      </button>
+      <ul
+        className={`${
+          isPagesOpened ? "left-0.5" : "-left-5"
+        } block justify-around items-center absolute top-15`}
+      >
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Forums</a>
+        </li>
+        <li>
+          <a href="#">FAQ</a>
+        </li>
+      </ul>
+
+      <button
+        onClick={() => setIsSocialsOpened(!isSocialsOpened)}
+        className="bg-primary p-1 rounded-xs"
+      >
+        socials
+      </button>
+      <ul className={`${isSocialsOpened ? "block" : "hidden"} items-center`}>
+        <li>
+          <a href="#">
+            <Image
+              className="aspect-4/5 object-contain dark:invert"
+              src={discordIcon}
+              width={25}
+              height={25}
+              alt={`discord png`}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <Image
+              className="aspect-9/10 object-contain dark:invert"
+              src={facebookIcon}
+              width={25}
+              height={25}
+              alt={`facebook png`}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <Image
+              className="aspect-4/5 object-contain dark:invert"
+              src={emailIcon}
+              width={25}
+              height={25}
+              alt={`email png`}
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Nav;
